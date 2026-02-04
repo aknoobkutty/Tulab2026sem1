a0066ddd8222e1ef48aebc32d6bced1aae4a5b98
Ak Noob Kutty <aknoobkutty@gmail.com>
2026-02-04 23:19:18 +0530

    Update 9  dbms pgms.sql

--- 9  dbms pgms.sql
+++ 9  dbms pgms.sql
@@ -1,51 +1,27 @@
-use dbmslab
-create table employees(
-employeeID INT PRIMARY
-KEY, employeename
-VARCHAR(50), managerID
-INT,
-FOREIGN KEY (managerID) REFERENCES employees(employeeID)
+Create schema 9pgm
+use 9pgm
+CREATE TABLE Employees (
+    EmployeeID INT PRIMARY KEY,
+    EmployeeName VARCHAR(50),
+    ManagerID INT,
+    FOREIGN KEY (ManagerID) REFERENCES Employees(EmployeeID)
 );
-INSERT INTO employees (employeeID, employeename, managerID)
-VALUES(1, 'rahul(manager)', NULL),
-(2, 'priya', 1),
-(3, 'karan', 1),
-(4, 'sneha', 2),
-(5, 'vijay', 2),
-(6, 'arun', 3);
-WITH RECURSIVE subordinates AS (
-SELECT employeeID, employeename,
-managerID FROM employees
-WHERE managerID = 1
-UNION ALL
-SELECT e.employeeID, e.employeename,
-e.managerID FROM employees e
-INNER JOIN subordinates s
-ON e.managerID = s.employeeID
-)
-SELECT * FROM subordinates;
-employeeID int primary key,
-employeename varchar(50),
- managerID int,
-foreignkey(managerID)references employees(employeeID)
-);
-insert into employees(employeeID,employeename,managerID) values
-(1,'rahul(manager)',null),
-(2,'priya',1),
-(3,'karan',1),
-(4,'sneha',2),
-(5,'vijay',2),
-(6,'arun',3);
-with recursive suborinates as(
-select
-employeeID,employeename,managerI
-D from employees
-where
-managerID=1
-union all
-select
-e.employeeID,e.employeename,e.managerI
-D from employees e
-inner join sunordinates son e.managerID=s.employeeID)
-select * from subordinates;
 
+INSERT INTO Employees VALUES
+(1,'Rahul',NULL),
+(2,'Priya',1),
+(3,'Karan',1),
+(4,'Sneha',2),
+(5,'Vijay',2),
+(6,'Arun',3);
+
+WITH RECURSIVE Subordinates AS (
+    SELECT EmployeeID, EmployeeName, ManagerID
+    FROM Employees WHERE ManagerID=1
+    UNION ALL
+    SELECT e.EmployeeID, e.EmployeeName, e.ManagerID
+    FROM Employees e
+    JOIN Subordinates s ON e.ManagerID = s.EmployeeID
+)
+select * From employees;
+SELECT * FROM Subordinates;
